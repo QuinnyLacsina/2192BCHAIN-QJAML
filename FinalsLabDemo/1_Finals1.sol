26ca2ac9308dfa0dc97d05cb1630b148c7079f02
QuinnyLacsina <98503897+QuinnyLacsina@users.noreply.github.com>
2024-03-19 14:13:19 +0800

    Add files via upload

---
+++ FinalsLabDemo/1_Finals1.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract FallbackExample {
+    event FallbackReceived(address sender, uint amount);
+    //Event to log payment received 
+    event PaymentReceived(address payer, uint256 amount);
+
+    //Fallback  function
+    fallback() external payable { 
+        emit FallbackReceived(msg.sender, msg.value);
+    }
+
+    //Function to receive Ether
+    receive() external payable { 
+        emit PaymentReceived(msg.sender, msg.value);
+    }
+}
+
+//Lacsina, Quinny Joyce Ann M.
+//These function ensures the contracts can respond appropriately to various type of Ether transactions, 
+//improving the reliability and usability of Ethereum smart contracts.
